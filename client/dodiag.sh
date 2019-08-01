6a62781ed55edeeb29a2a25a055fe290f41fcdb7
glitchub <glitchub@users.noreply.github.com>
2019-08-01 14:39:17 -0700

    wip

--- client/dodiag.sh
+++ client/dodiag.sh
@@ -7,8 +7,8 @@
 # Abort with message
 die() { echo $* >&2; exit 1; }
 
-[ $# = 3 ] || die "Usage: dodiag.sh pionicIP serverIP buildID"
-pionicIP=$1 serverIP=$2 buildID=$3
+[ $# = 2 ] || die "Usage: dodiag.sh pionicIP buildID"
+pionicIP=$1 buildID=$2
 
 # The current directory contains the downloaded diagnostics, make sure that the
 # dodiag program is there.
@@ -18,13 +18,11 @@ pionicIP=$1 serverIP=$2 buildID=$3
 # drivers, etc, in preparation for performing diagnostics.
 
 # XXX get the device ID from permanent storage here. If device ID is not
-# currently programmed then leave as "" and the system will scan the barcode
-# from the PCB and then perform phase 1 tests (which must include installation
-# of the device ID).
+# currently programmed then leave as "" and the system will try to scan the
+# barcode from the PCB and then perform phase 1 tests (which must include
+# installation of the device ID).
 deviceID=""
 
-# Now invoke dodiag 
-./dodiag -p $pionicIP -s $serverIP $buildID $deviceID
-exs=$?
-echo "dodiag exit with status $exs"
-exit $exs
+# Now invoke dodiag
+./dodiag -p $pionicIP $buildID $deviceID
+die "dodiag exit with status $?"
--- client/startdiag.sh
+++ client/startdiag.sh
@@ -5,8 +5,10 @@
 # to download a tarball from the factory server, then passes control to
 # dodiag.sh from the tarball.
 
-# This is the pionic controller's IP address
-pionicIP=192.168.111.1  # pionic controller IP address
+# This is the pionic controller's IP address. Note pionic forwards port 61080
+# to the factory server port 80, and 61443 to factory server port 443. All
+# server traffic should go via these ports.
+pionic=192.168.111.1  # pionic controller IP address
 
 # This is the factory detction method. If the DUT has a pre-defined static IP
 # address at the time that this script runs, then use 'http'. Otherwise use
@@ -20,7 +22,7 @@ interface=
 # If 'beacon', the IP address to apply to the interface after beacon is
 # detected (in the same subnet as pionic). Not required for http since the
 # network must already be up.
-localIP=
+address=
 
 # The factory server has a self-signed https certificate. We verify the cert
 # using the base-64 encoded sha256 of the public key. For reference, this can
@@ -51,64 +53,67 @@ localIP=
 development="paYQewbP520iAv1hIi/A1lvYyVzMdDv6yEmp9El0aPc="
 production=""
 
-# We assume it's not possible to properly recover from diagnostic mode without
-# a full reboot, therefore this script spins forever on any non-zero exit
-# status.
-trap '[ $? != 0 ] && while true; do echo "Reboot now"; sleep 5; done' EXIT
-
 # die with message to stderr
 die() { echo $* >&2; exit 1; }
 
-# First, try to detect the factory.
-if [ $method == beacon ]; then
-    # Beacon mode, listen for an ethernet beacon. If we don't hear one then
-    # assume we're not in the factory and just exit
-    beacon recv 2 $interface pionic >/dev/null || exit 0
-    # There is no exit after this point
-    echo "Beacon detected, configuring $interface for $localIP"
-    ip link set $interface up || true
-    ip address add $localIP dev $interface || true
-    echo "Fetching server IP from pionic"
-    serverIP=$(curl -qsf -m3  "http://$pionicIP/factory") || die "Failed"
-else
-    # http mode, we assume $pionicIP is on the local subnet. Ask it for the
-    # factory IP address, if it fails then assume we're not in the factory and
-    # just exit.
-    serverIP=$(curl -qsf -m3  "http://$pionicIP/factory") || exit 0
-    # There is no exit after this point
-fi
+curl="curl -qsSf"
 
-echo "Factory server is $serverIP"
-# XXX set $pionicIP as default gateway if necessary
+# First, try to detect the factory.
+case "$method" in
+    beacon)
+        # Listen for an ethernet beacon. If we don't hear one then
+        # assume we're not in the factory and just exit
+        beacon recv 2 $interface pionic >/dev/null || exit 0
+        # There is no exit after this point
+        echo "Beacon detected, configuring $interface for $address"
+        ip link set $interface up || true
+        ip address add $address dev $interface || true
+        ;;
+    
+    http)        
+        # We expect that $pionic is on the local subnet and forwards port 61080
+        # to the server http, and 61433 to the server https. Perform a server
+        # test connect, if it doesn't work then assume we're not in the factory
+        # and just exit.
+        $curl -m 2 "http://$pionic:61080/factory?service=test" &>/dev/null || exit 0
+        ;;
+    
+    *)
+        die "Invalid method $method"
+        ;;
+esac        
+
+# If we're here, then we're in the factory. Make sure the only way out is with
+# a reboot.
+trap 'while true; do echo "Reboot now"; sleep 5; done' EXIT
 
 # XXX set the build ID. The build ID must uniquely identify the software build
-# and the DUT device type. Note the server associates the buildID with a
-# specific tarball containing the diagnostic code, therefore the buildID string
-# must be predictable and human-writable.
-buildID=test
-echo "Using buildID $buildID"
+# and the DUT device type. Note the server associates the build ID with a
+# specific tarball containing the diagnostic code, therefore the build string
+# must be predictable and readable by humans.
+build=test
+echo "Using buildID $build"
 
 # XXX create a work directory. The work directory must not survive reboot, if
-# necessary mount a ramdisk/tmpfs and cd to that. Note the directory must be
-# large enough to contain the contents of the diagnostic tarball. 256MB is a
-# good target. The exact path doesn't matter.
+# necessary mount a ramdisk/tmpfs and cd to that. The directory must be large
+# enough to contain the contents of the diagnostic tarball, 256MB is a good
+# target. The exact path doesn't matter.
 echo "Creating work directory"
 mkdir /tmp/diag
 cd /tmp/diag
 
-# Retrieve the diagnostic tarball from the server and unpack it into the
-# current directory. Note we verify the hash of the https certificate public
-# key, so we can trust that the tarball really did come from the factory server
-# and not from someone's laptop. If both hashes are defined, try the production
-# hash first. The development hash MUST NOT be defined for production software,
-# see the note above.
+# Retrieve the diagnostic tarball from the server https (via pionic port 61443)
+# and unpack it into the current directory. Verify the hash of the https
+# certificate public key, so we can trust that the tarball really did come from
+# the factory server and not from someone's laptop. If both hashes are defined,
+# try the production hash first. The development hash MUST NOT be defined for
+# production software, see the note above.
 for pubkey in ${production:-} ${development:-}; do
     echo "Downloading with key hash $pubkey"
-    if curl -qsSf -k --pinnedpubkey "sha256//$pubkey" --form-string "buildid=$buildID" "https://$server/cgi-bin/factory?service=download" | tar -xzv; then
-        # Success!
+    if $curl -k --pinnedpubkey "sha256//$pubkey" --form-string "buildid=$build" "https://$pionic:61443/cgi-bin/factory?service=download" | tar -xzv; then
         [ -x ./dodiag.sh ] || die "Tarball does not contain dodiag.sh"
         # invoke with various parameters of interest
-        ./dodiag.sh $pionicIP $serverIP $buildID
+        ./dodiag.sh $pionic $build
         die "dodiag.sh exit with status $?"
     fi
 done
